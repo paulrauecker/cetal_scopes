b8b4e77e60bf269a838d3febf6e9fdfaae868ffb
Dorin Molovata <dorinmolovata@gmail.com>
2026-09-04 15:57:45 +0300

    initial commit

--- src/cetal_scopes/__init__.py
+++ src/cetal_scopes/__init__.py
@@ -1,2 +1,19 @@
+
+
 def hello() -> str:
     return "Hello from cetal-scopes!"
+
+
+def goodbye() -> str:
+    return "Goodbye from cetal-scopes!"
+
+def whatname() -> str:
+    return __name__
+
+
+
+
+
+if __name__ == "__main__":
+    print(hello())
+    print(goodbye())
